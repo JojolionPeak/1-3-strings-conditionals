7914a6365b82ac28b387cd794851a0f65b6fe885
Jonathan <JohnAnBer@gmail.com>
2025-10-08 12:04:38 -0400

    class on 10/8

--- 2-bracket-notation.js
+++ 2-bracket-notation.js
@@ -1,17 +1,19 @@
 // Bracket notation evaluates to a single character
-console.log("abc"[0]);    // Output: a
-console.log("abc"[1]);    // Output: b
-console.log("abc"[2]);    // Output: c
+// console.log("abc"[0]);    // Output: a
+// console.log("abc"[1]);    // Output: b
+// console.log("abc"[2]);    // Output: c
 
 
-// It works on variables too!
-const message = 'Hello there!';
+// // It works on variables too!
+//  let message = 'Hello there!';
 
-console.log(message[0]);  // Output: H
-console.log(message[4]);  // Output: o
-console.log(message[5]);  // Output: ???
-console.log(message[11]); // Output: ???
+// // console.log(message[0]);  // Output: H
+// // console.log(message[4]);  // Output: o
+// // console.log(message[5]);  // Output: space
+// // console.log(message[11]); // Output: !
 
 
-// undefined is returned if the index is invalid
-console.log(message[50]); // Output: undefined
+// // undefined is returned if the index is invalid
+// // console.log(message[50]); // Output: undefined
+// message[0] = "J"
+// console.log(message);
--- 3-length.js
+++ 3-length.js
@@ -1,11 +1,14 @@
+let message = 'Hello there!';
 console.log(message.length);
 // Output: 12
+// find the last index number syntax
+//string.length - 1
 
 console.log(message[message.length - 1]);
-// Output: ! 
+// Output: !
 
 console.log(message[message.length - 2]);
-// Output: e 
+// Output: e
 
 console.log(message[message.length - 12]);
-// Output: H  
+// Output: H
